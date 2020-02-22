b8f270f24b63bee087afd2643e8c7aa7cff0e7d3
theOpetunde <opetundeadepoju@gmail.com>
2020-02-22 13:39:47 +0100

    Added profiles view set and permissions file

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView #APIView is a class of the rest_framewo
 from rest_framework.response import Response #returns responses from the APIView
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
-
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -89,3 +91,11 @@ class HelloViewSet(viewsets.ViewSet):
     def dstroy(self, request, pk=None):
         """Handle deleting/removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
